25dd7a42c0cab9e6f20aeaeab160da7b0e2c03e3
pdMM11 <46798433+pdMM11@users.noreply.github.com>
2020-11-26 18:00:27 +0000

    Update clustal.component.ts

--- Angular_/ngx-admin/src/app/pages/tools-webserver/clustal/clustal.component.ts
+++ Angular_/ngx-admin/src/app/pages/tools-webserver/clustal/clustal.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
+// import {WeblogoService} from "../../../services/weblogo.service";
+import {ProteinService} from '../../../services/protein.service';
+import {ClustalService} from '../../../services/clustal.service';
 
 @Component({
   selector: 'ngx-clustal',
@@ -19,11 +22,31 @@ export class ClustalComponent implements OnInit {
   ];
   option = ' ';
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private clustal: ClustalService,
+              private protein: ProteinService) {
   }
 
   ngOnInit() {
-    this.query_seq = this.route.snapshot.queryParamMap.get('sequence');
+    const query_seq_id = this.route.snapshot.queryParamMap.get('sequence');
+
+    const seqs_id = query_seq_id.split(',');
+
+    // let seqs_data = '';
+
+    for (let i = 0; i < seqs_id.length; i++) {
+      // seqs_data = seqs_data + '>' + seqs_id[i] + '\n';
+      this.protein.getPage(1, 'Prot', '', seqs_id[i]).subscribe((data: Array<object>) => {
+        this.aux_query_seq(seqs_id[i], data['results'][0]['sequence_fusogenic']);
+      });
+      // seqs_data = seqs_data + this.aux_data + '\n';
+      // alert(seqs_data);
+    }
+    // this.query_seq = this.aux_data;
+  }
+
+  aux_query_seq(seqid, data) {
+    this.query_seq = this.query_seq + '>' + seqid + '\n' + data + '\n';
   }
 
   gotoEBI(): void {
@@ -48,7 +71,40 @@ export class ClustalComponent implements OnInit {
         '&ktuple=1&window=5&pairgap=3&topdiags=5&score=PERCENT&pwgapopen=10.0&pwgapext=0.1' +
         '&matrix=' + this.selectedItemMat, '_blank');
     } else {
-      alert('ERROR: MULTIPLE SEQUENCE NEEDS TO BE FILLED in FASTA FORMAT');
+      alert('ERROR: MULTIPLE SEQUENCE NEEDS TO BE FILLED IN FASTA FORMAT');
+    }
+  }
+
+  gotoClustal(): void {
+    let seqSend = '';
+    if (this.sequence.value !== null && this.sequence.value !== ''
+      && this.sequence.value.includes('\n') && this.sequence.value.includes('>')) {
+      seqSend = this.sequence.value;
+    }  else if (this.query_seq !== null && this.query_seq !== ''
+      && this.query_seq.includes('\n') && this.query_seq.includes('>')) {
+      seqSend = this.query_seq;
+    } else {
+      alert('ERROR: MULTIPLE SEQUENCE NEEDS TO BE FILLED IN FASTA FORMAT');
+    }
+    if (seqSend !== '') {
+      this.clustal.clustalBackend(seqSend, this.option).subscribe(data => {
+        const data_result = data as string;
+        const blob = new Blob([data_result], {type: 'text/plain'});
+        const url = window.URL.createObjectURL(blob);
+        window.open(url);
+      }, error => {
+        alert('ERROR');
+      });
+      this.clustal.clustalTreeBackend().subscribe(data => {
+        const data_result = data as string;
+        alert(data_result);
+        const blob = new Blob([data_result], {type: 'text/plain'});
+        const url = window.URL.createObjectURL(blob);
+        window.open(url);
+      }, error => {
+        alert('ERROR');
+      });
     }
   }
 }
+
